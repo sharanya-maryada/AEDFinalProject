afc6a33127bdfe36ac33fe7d4441fd8769cc4ad7
sharanya-maryada <maryada.s@northeastern.edu>
2022-04-25 12:31:04 -0400

    enterprisedirectory

---
+++ AEDFinalProject/src/main/java/Backend/Enterprise/EnterpriseDirectory.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Backend.Enterprise;
+
+import Backend.Organization.OrganizationDirectory;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author ms
+ */
+public class EnterpriseDirectory {
+    private ArrayList<Enterprise> enterpriseList;
+   
+
+    public ArrayList<Enterprise> getEnterpriseList() {
+        return enterpriseList;
+    }
+
+    public void setEnterpriseList(ArrayList<Enterprise> enterpriseList) {
+        this.enterpriseList = enterpriseList;
+    }
+    
+    public EnterpriseDirectory(){
+        enterpriseList=new ArrayList<Enterprise>();
+    }
+    
+    //Create enterprise
+    public Enterprise createAndAddEnterprise(String name,Enterprise.EnterpriseType type){
+        Enterprise enterprise=null;
+        if(type==Enterprise.EnterpriseType.OrganBank){
+            enterprise=new OrganBankEnterprise(name);
+            enterpriseList.add(enterprise);
+        }
+        else if(type==Enterprise.EnterpriseType.LegalDepartment){
+            enterprise=new LegalDepartmentEnterprise(name);
+            enterpriseList.add(enterprise);
+        }
+        else if(type==Enterprise.EnterpriseType.HospitalDepartment){
+            enterprise = new HospitalDepartmentEnterprise(name);
+            enterpriseList.add(enterprise);
+        }
+        return enterprise;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
