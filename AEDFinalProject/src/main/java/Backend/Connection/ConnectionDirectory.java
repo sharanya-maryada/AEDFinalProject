e199fcfe2a4a68a28fcc9611e967daba1aa2169d
sharanya-maryada <maryada.s@northeastern.edu>
2022-04-25 12:25:44 -0400

    connectiondirectory

---
+++ AEDFinalProject/src/main/java/Backend/Connection/ConnectionDirectory.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Backend.Connection;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author ms
+ */
+public class ConnectionDirectory {
+    
+    private ArrayList<Connection> connectionList;
+    
+    
+     public ConnectionDirectory() {
+        connectionList = new ArrayList();
+    }
+
+    public ArrayList<Connection> getConnectionList() {
+        return connectionList;
+    }
+
+  
+       
+
+
+    
+    
+}
