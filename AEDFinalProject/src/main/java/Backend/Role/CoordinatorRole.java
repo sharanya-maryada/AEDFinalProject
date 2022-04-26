dea43b8a51f9a3fb71187fac7c82ff7fdbe65754
sharanya-maryada <maryada.s@northeastern.edu>
2022-04-26 14:40:10 -0400

    coordrole

---
+++ AEDFinalProject/src/main/java/Backend/Role/CoordinatorRole.java
@@ -0,0 +1,34 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Backend.Role;
+
+import Backend.DB4OUtil.DB4OUtil;
+import Backend.EcoSystem;
+import Backend.Enterprise.Enterprise;
+import Backend.Organization.Organization;
+import Backend.UserAccount.UserAccount;
+import UI.Admin.AdminDashBoardJPanel;
+import UI.Coordinator.CoordinatorDashBoardJPanel;
+import java.awt.CardLayout;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import UI.Coordinator.CoordinatorManageRequestsJPanel;
+
+/**
+ *
+ * @author ms
+ */
+public class CoordinatorRole extends Role {
+
+    @Override
+   public void createWorkArea(JPanel userProcessContainer, UserAccount account, Organization organization, Enterprise enterprise, EcoSystem business,DB4OUtil dB4OUtil) {
+        CoordinatorDashBoardJPanel lab = new CoordinatorDashBoardJPanel(account,organization,business,dB4OUtil,enterprise);
+        userProcessContainer.add("CoordinatorDashBoardJPanel",lab);
+        CardLayout layout = (CardLayout) userProcessContainer.getLayout();
+        layout.next(userProcessContainer);
+        lab.setVisible(true);
+    }
+    
+}
