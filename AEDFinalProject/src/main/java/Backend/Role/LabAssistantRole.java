1b7acb62d4eab648284c4e1255af258146e9bce1
sharanya-maryada <maryada.s@northeastern.edu>
2022-04-26 14:40:40 -0400

    labassistant

---
+++ AEDFinalProject/src/main/java/Backend/Role/LabAssistantRole.java
@@ -0,0 +1,35 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Backend.Role;
+
+import Backend.DB4OUtil.DB4OUtil;
+import Backend.EcoSystem;
+import Backend.Enterprise.Enterprise;
+import Backend.Organization.Organization;
+import Backend.UserAccount.UserAccount;
+import UI.Admin.AdminDashBoardJPanel;
+import UI.Coordinator.CoordinatorDashBoardJPanel;
+import java.awt.CardLayout;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import UI.Coordinator.CoordinatorManageRequestsJPanel;
+import UI.LabAssistant.LabAssistantDashBoardJPanel;
+
+/**
+ *
+ * @author ap
+ */
+public class LabAssistantRole extends Role {
+
+    @Override
+   public void createWorkArea(JPanel userProcessContainer, UserAccount account, Organization organization, Enterprise enterprise, EcoSystem business,DB4OUtil dB4OUtil) {
+       LabAssistantDashBoardJPanel lab = new LabAssistantDashBoardJPanel(account,organization,business,dB4OUtil,enterprise);
+        userProcessContainer.add("LabAssistantDashBoardJPanel",lab);
+        CardLayout layout = (CardLayout) userProcessContainer.getLayout();
+        layout.next(userProcessContainer);
+        lab.setVisible(true);
+    }
+    
+}
