5e38228ecb417fc9a6c1b129c8b8ec930d07af06
sharanya-maryada <maryada.s@northeastern.edu>
2022-04-26 14:41:08 -0400

    sysadmin

---
+++ AEDFinalProject/src/main/java/Backend/Role/SystemAdminRole.java
@@ -0,0 +1,34 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Backend.Role;
+
+import Backend.DB4OUtil.DB4OUtil;
+import Backend.EcoSystem;
+import Backend.Enterprise.Enterprise;
+import Backend.Organization.Organization;
+import Backend.UserAccount.UserAccount;
+import UI.Admin.AdminDashBoardJPanel;
+import UI.SysAdmin.SysAdminDashboardJPanel;
+import java.awt.CardLayout;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+
+/**
+ *
+ * @author ap
+ */
+public class SystemAdminRole extends Role{
+
+    @Override
+    public void createWorkArea(JPanel userProcessContainer, UserAccount account, Organization organization, Enterprise enterprise, EcoSystem system,DB4OUtil dB4OUtil) {
+        SysAdminDashboardJPanel jpanel = new SysAdminDashboardJPanel(system,dB4OUtil);
+        userProcessContainer.add("SysAdminDashboardJPanel",jpanel);
+        CardLayout layout = (CardLayout) userProcessContainer.getLayout();
+        layout.next(userProcessContainer);
+        jpanel.setVisible(true);
+    }
+    
+}
